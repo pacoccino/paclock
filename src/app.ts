adf8c5bdd6823757f1f2111f71b7c844a5256778
paco <pakokrew@gmail.com>
2022-08-02 12:52:28 +0200

    sun algorithm

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import P5 from 'p5'
 
 import { clockSketch } from './sketch'
 import { Clock } from './clock'
+import { Test } from './lib/sun.test'
 
 async function App() {
+  Test()
   const clock = new Clock()
   clock.start()
   new P5(clockSketch(clock), document.getElementById('sketch'))
---
+++ src/lib/sun.test.ts
@@ -0,0 +1,30 @@
+import moment from 'moment'
+
+import { Sun, Utils, Location } from './sun'
+
+export function Test() {
+  const date = moment.utc({
+    year: 2022,
+    month: 7,
+    day: 2,
+    hour: 12,
+    minutes: 0,
+    seconds: 0,
+  })
+  const location: Location = {
+    lat: 52.33,
+    long: 13.3,
+  }
+  const tz = 2
+
+  console.log('date: ', date.format())
+
+  const result = Sun.calculate(date, location, tz)
+
+  console.log('res', result)
+
+  const sunnoon = Utils.minutesToHour(result.solnoon)
+  const sunrise = Utils.minutesToHour(result.rise.timelocal)
+  const sunset = Utils.minutesToHour(result.set.timelocal)
+  console.log(sunrise, sunset, sunnoon)
+}
