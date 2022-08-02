2f9bda412b280261552bf5a213345b6d7612f318
paco <pakokrew@gmail.com>
2022-08-02 10:50:01 +0200

    theming

---
+++ src/themes.ts
@@ -0,0 +1,20 @@
+export const Themes = {
+  bw: {
+    background: 15,
+    foreground: 240,
+    f200: 200,
+    f400: 160,
+    f600: 100,
+    night: 15,
+    day: 240,
+  },
+  yb: {
+    background: 15,
+    foreground: 240,
+    f200: 200,
+    f400: 160,
+    f600: 100,
+    night: '#0d2980',
+    day: '#de9610',
+  },
+}
