34e346db2ad4cc98b918c56b4159d2f2234958d8
paco <pakokrew@gmail.com>
2022-08-02 13:29:40 +0200

    apply to clock and geoloc

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import P5 from 'p5'
 
 import { clockSketch } from './sketch'
 import { Clock } from './clock'
-import { Test } from './lib/sun.test'
+// import { Test } from './lib/sun.test'
 
 async function App() {
-  Test()
+  //Test()
   const clock = new Clock()
   clock.start()
   new P5(clockSketch(clock), document.getElementById('sketch'))
