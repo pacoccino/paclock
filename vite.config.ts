75f1ca708fa30c8b87d5b6460007e40d4f4e34b6
paco <pakokrew@gmail.com>
2022-09-11 17:39:50 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       },
       injectRegister: 'auto',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
       },
     }),
   ],
