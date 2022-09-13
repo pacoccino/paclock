a1ad47c0c815a3e62260c427198ddc8f0f4cd2dc
paco <pakokrew@gmail.com>
2022-09-13 22:35:04 +0200

    no time

--- src/sketch/texts.ts
+++ src/sketch/texts.ts
@@ -9,13 +9,13 @@ export function drawText(p: P5, clock: Clock, responsive: Responsive) {
 
   p.translate(p.width / 2, p.height / 2)
 
+  /*
   p.noStroke()
   p.textAlign(p.CENTER, p.CENTER)
   p.textSize(responsive.baseFontWeight * 4)
   p.fill(theme.foreground)
   p.text(`${clock.now.format('HH:mm')}`, 0, 0)
 
-  /*
   p.textSize(responsive.baseFontWeight * 1.5)
   p.fill(theme.f200)
   p.text(
@@ -25,19 +25,19 @@ export function drawText(p: P5, clock: Clock, responsive: Responsive) {
   )
 
 
-p.textSize(baseFontWeight)
-p.fill(theme.f400)
-p.text(
- [
-   // clock.location.district,
-   clock.location.zipcode,
-   clock.location.country_code2,
- ]
-   .filter((e) => !!e)
-   .join(', '),
- 0,
- -baseFontWeight * 4
-)
+  p.textSize(baseFontWeight)
+  p.fill(theme.f400)
+  p.text(
+   [
+     // clock.location.district,
+     clock.location.zipcode,
+     clock.location.country_code2,
+   ]
+     .filter((e) => !!e)
+     .join(', '),
+   0,
+   -baseFontWeight * 4
+  )
 */
   p.pop()
 }
